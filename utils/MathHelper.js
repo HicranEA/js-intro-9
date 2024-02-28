124b63c13b6bfe1d4f9698f97d53f3c4b96c8405
HicranEA <hicran_er@yahoo.com>
2024-02-28 13:36:37 -0600

    more loops

--- utils/MathHelper.js
+++ utils/MathHelper.js
@@ -5,7 +5,5 @@ function generateRandomNumber(a, b) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(generateRandomNumber(1, 10));
-
 // Exporting your reusable methods
 module.exports.generateRandomNumber = generateRandomNumber;
